4a974bcfa5c4ab95f4fe2c0e026cd5c81265e0b5
martuafernando <fernandosibarani45@gmail.com>
2022-12-07 23:01:27 +0700

    add data binding

--- app/src/main/java/com/example/healthy/MainActivity.java
+++ app/src/main/java/com/example/healthy/MainActivity.java
@@ -4,27 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import com.example.healthy.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     ImageView dailyhealth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
         getSupportActionBar().hide();
 
-        dailyhealth = findViewById(R.id.dailyQuestion);
-
-        dailyhealth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Questioner.class));
-            }
-        });
+//        dailyhealth = findViewById(R.id.dailyQuestion);
+//
+//        dailyhealth.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(MainActivity.this, Questioner.class));
+//            }
+//        });
     }
 }
