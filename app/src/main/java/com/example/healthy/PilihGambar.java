05999f77d6bcad08865bd69ec86addf412363761
martuafernando <fernandosibarani45@gmail.com>
2022-12-10 16:39:20 +0700

    add image picker in decode pesan page

--- app/src/main/java/com/example/healthy/PilihGambar.java
+++ app/src/main/java/com/example/healthy/PilihGambar.java
@@ -1,12 +1,22 @@
 package com.example.healthy;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 
 import com.example.healthy.databinding.ActivityEncodeMessageBinding;
 import com.example.healthy.databinding.ActivityPilihGambarBinding;
+
+import java.io.File;
 
 public class PilihGambar extends AppCompatActivity {
     private ActivityPilihGambarBinding binding;
@@ -26,5 +36,32 @@ public class PilihGambar extends AppCompatActivity {
                 finish();
             }
         });
+
+        binding.pilihGambarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                Uri uri = Uri.parse(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + File.separator + "Secreto" + File.separator);
+                intent.setDataAndType(uri, "image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                someActivityResultLauncher.launch(intent);
+            }
+        });
     }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        if (data != null && data.getData() != null) {
+                            Uri selectedImageUri = data.getData();
+                            binding.pilihGambarButton.setImageURI(selectedImageUri);
+                        }
+                    }
+                }
+            }
+    );
 }
