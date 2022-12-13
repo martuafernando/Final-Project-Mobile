3c453113d1f1d2548acfa51a0b3d0a2d66a743f7
martuafernando <fernandosibarani45@gmail.com>
2022-12-13 09:19:30 +0700

    fixing API for EncodeMessage

---
+++ app/src/main/java/com/example/healthy/API.java
@@ -0,0 +1,7 @@
+package com.example.healthy;
+
+public final class API {
+
+    public static String domain = "http://192.168.100.13:3000";
+    public static String encodeMessage = domain + "/message";
+}
