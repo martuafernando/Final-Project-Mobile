84439e9a6ca6cf5f29399c14b42a88afddcbfe16
martuafernando <fernandosibarani45@gmail.com>
2022-12-13 09:37:36 +0700

    fixing API for DaftarPesan

--- app/src/main/java/com/example/healthy/API.java
+++ app/src/main/java/com/example/healthy/API.java
@@ -4,4 +4,5 @@ public final class API {
 
     public static String domain = "http://192.168.100.13:3000";
     public static String encodeMessage = domain + "/message";
+    public static String daftarPesan = domain + "/message";
 }
--- app/src/main/java/com/example/healthy/DaftarPesan.java
+++ app/src/main/java/com/example/healthy/DaftarPesan.java
@@ -8,6 +8,18 @@ import android.view.View;
 
 import com.example.healthy.databinding.ActivityDaftarPesanBinding;
 import com.example.healthy.databinding.ActivityMainBinding;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class DaftarPesan extends AppCompatActivity {
 
@@ -19,6 +31,30 @@ public class DaftarPesan extends AppCompatActivity {
         binding = ActivityDaftarPesanBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
+
+        OkHttpClient client = new OkHttpClient();
+
+        Picasso.get().load("https://i.imgur.com/DvpvklR.png").into(binding.gambar1);
+
+        Request request = new Request.Builder()
+                .url(API.encodeMessage)
+                .method("GET", null)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+                System.out.println("testing::" + e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                final String myResponse = response.body().string();
+                System.out.println("testing::" + myResponse);
+            }
+        });
 
         binding.decodePesanButton.setOnClickListener(new View.OnClickListener() {
             @Override
